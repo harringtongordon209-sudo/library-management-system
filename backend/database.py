f956593a30847c943c4f3ba8b4311afb7856b99b
Gordon Harrington <harringtongordon209@gmail.com>
2026-08-23 21:08:54 +0100

    Refactor code to use router approach for endpoint

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Generator
+import redis
 
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 SQLALCHEMY_DATABASE_URL = "postgresql://library_admin:local_password_123@localhost:5432/library_db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
